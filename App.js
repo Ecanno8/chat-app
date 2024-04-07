aac7afd4d5e1570d07eb2727578f2ff292ccdac2
Ecanno8 <ethancannon92@gmail.com>
2024-04-07 04:01:10 -0700

    chat-app

--- App.js
+++ App.js
@@ -1,37 +1,31 @@
-import { useState } from 'react';
-import { StyleSheet, View, Text, TextInput } from 'react-native';
+// import the screens
+import Screen1 from './components/Screen1';
+import Screen2 from './components/Screen2';
+
+// import react Navigation
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+// Create the navigator
+const Stack = createNativeStackNavigator();
 
 const App = () => {
-  const [text, setText] = useState('');
-
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.textInput}
-        value={text}
-        onChangeText={setText}
-        placeholder='Type Something Here'
-      />
-      <Text style={styles.textDisplay}>You wrote: {text}</Text>
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator
+        initialRouteName="Screen1"
+      >
+        <Stack.Screen
+          name="Screen1"
+          component={Screen1}
+        />
+        <Stack.Screen
+          name="Screen2"
+          component={Screen2}
+        />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center'
-  },
-  textInput: {
-    width: '88%',
-    borderWidth: 1,
-    height: 50,
-    padding: 10
-  },
-  textDisplay: {
-    height: 50,
-    lineHeight: 50
-  }
-});
 
 export default App;
