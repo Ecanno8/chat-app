92015fac834213486f5e981cdd2cfa4896c19a41
Ecanno8 <ethancannon92@gmail.com>
2024-04-07 03:44:27 -0700

    chatApp

--- App.js
+++ App.js
@@ -1,14 +1,36 @@
-import { StyleSheet, View } from 'react-native';
+import { useState } from 'react';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
 
 const App = () => {
+  const [text, setText] = useState('');
+
   return (
-    <View style={styles.container}></View>
+    <View style={styles.container}>
+      <TextInput
+        style={styles.textInput}
+        value={text}
+        onChangeText={setText}
+        placeholder='Type Something Here'
+      />
+      <Text style={styles.textDisplay}>You wrote: {text}</Text>
+    </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
-    flex: 1
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center'
+  },
+  textInput: {
+    width: '88%',
+    borderWidth: 1,
+    height: 50,
+    padding: 10
+  },
+  textDisplay: {
+    height: 50,
+    lineHeight: 50
   }
 });
 
