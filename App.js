e1db751c61bcd2f29fedd6fbc59dc7e4cdd6e2db
Ecanno8 <ethancannon92@gmail.com>
2024-04-07 03:31:29 -0700

    chatApp clean slate

--- App.js
+++ App.js
@@ -1,31 +1,15 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View } from 'react-native';
 
 const App = () => {
   return (
-    <View style={styles.container}>
-      <Text>Hello World!</Text>
-      <Text>How are you?</Text>
-      <Text>I'm feeling blue!</Text>
-    </View>
+    <View style={styles.container}></View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  blue: {
-    color: 'blue',
-    fontWeight: '600',
-  },
-  bigRed: {
-    color: 'red',
-    fontSize: 30,
-  },
+    flex: 1
+  }
 });
 
 export default App;
