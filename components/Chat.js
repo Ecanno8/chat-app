36cfca3d0e89c285ef948ab3a4dd826024eba56b
Ecanno8 <ethancannon92@gmail.com>
2024-04-09 16:06:59 -0700

    5.2 giftedchat,kbview, acessibility

--- components/Chat.js
+++ components/Chat.js
@@ -1,16 +1,68 @@
 import { useEffect } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Platform, KeyboardAvoidingView, StyleSheet } from 'react-native';
+import { Bubble, GiftedChat } from "react-native-gifted-chat";
 
 const Chat = ({ route, navigation }) => {
     const { name, background } = route.params;
+    const [messages, setMessages] = useState([])
+
+    // for sending messages
+    const onSend = (newMessages) => {
+        setMessages(previousMessages => GiftedChat.append(previousMessages, newMessages))
+    }
+
+    // for customazing your messages
+    const renderBubble = (props) => {
+        return <Bubble
+            {...props}
+            wrapperStyle={{
+                right: {
+                    backgroundColor: '#2a9d8f',
+                },
+                left: {
+                    backgroundColor: '#fff'
+                }
+            }}
+        />
+    }
 
     useEffect(() => {
         navigation.setOptions({ title: name });
-    }, []);
+    }, [])
+    // default messages that appear when you get to the chat page
+    useEffect(() => {
+        setMessages([
+            {
+                _id: 1,
+                text: 'Hello developer',
+                createdAt: new Date(),
+                user: {
+                    _id: 2,
+                    name: 'React Native',
+                    avatar: 'https://placeimg.com/140/140/any'
+                },
+            },
+            {
+                _id: 2,
+                text: 'This is a system message',
+                createdAt: new Date(),
+                system: true,
+            },
+        ])
+    }, [])
 
     return (
         <View style={[styles.container, { backgroundColor: background }]}>
-            <Text>Welcome to the chat</Text>
+            <GiftedChat
+                messages={messages}
+                renderBubble={renderBubble}
+                onSend={messages => onSend(messages)}
+                user={{
+                    _id: 1
+                }}
+            />
+            {/* so that the keyboard does not overlap the input  */}
+            {Platform.OS === 'android' ? <KeyboardAvoidingView behavior="height" /> : null}
         </View>
     );
 }
@@ -18,10 +70,6 @@ const Chat = ({ route, navigation }) => {
 const styles = StyleSheet.create({
     container: {
         flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-        width: '100%',
-        height: '100%',
     }
 });
 
--- components/Start.js
+++ components/Start.js
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { View, Text, TouchableOpacity, TextInput, ImageBackground, StyleSheet } from 'react-native';
+import { View, Text, TouchableOpacity, TextInput, ImageBackground, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
 
 const Start = ({ navigation }) => {
     const [name, setName] = useState('');
@@ -12,57 +12,65 @@ const Start = ({ navigation }) => {
                 <Text style={styles.appTitle}>Welcome!</Text>
                 <View style={styles.box}>
                     <TextInput
+                        accessibilityLabel="Username input"
+                        accessibilityRole="text"
                         style={styles.textInput}
                         value={name}
                         onChangeText={setName}
                         placeholder='Type your username here'
                     />
                     <Text style={styles.chooseBackgroundColor}>Choose Background Color</Text>
-                    {/* Choose background color of chat \*/}
+                    {/* Choose background color of chat */}
                     <View style={styles.colorButtonsBox}>
                         {colors.map((color, index) => (
                             <TouchableOpacity
+                                accessibilityLabel="Color Button"
+                                accessibilityHint="Lets you choose a backgroundcolor for your chat."
+                                accessibilityRole="button"
                                 key={index}
                                 style={[styles.colorButton, { backgroundColor: color }, background === color && styles.selected]}
                                 onPress={() => setBackground(color)}
                             />
                         ))}
                     </View>
-                    {/* Start Chat \*/}
-                    <TouchableOpacity style={styles.button} onPress={() => navigation.navigate('Chat', { name: name, background: background })}>
+                    {/* Start Chat */}
+                    <TouchableOpacity accessibilityLabel="Start Chatting"
+                        accessibilityRole="button"
+                        style={styles.button}
+                        onPress={() => navigation.navigate('Chat', { name: name, background: background })}>
                         <Text style={styles.buttonText}>Start Chatting</Text>
                     </TouchableOpacity>
                 </View>
+                {Platform.OS === "android" ? (
+                    <KeyboardAvoidingView behavior="height" />
+                ) : null}
             </ImageBackground >
         </View >
     );
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-    },
+
     bgImage: {
         flex: 1,
-        justifyContent: 'center',
         alignItems: 'center',
-        width: '100%',
-        height: '100%',
+
     },
     appTitle: {
         fontSize: 45,
         fontWeight: '600',
+        justifyContent: 'center',
+        alignItems: 'center',
         color: '#ffffff',
         margin: 20
     },
     box: {
         backgroundColor: '#ffffff',
-        padding: 30,
         width: '88%',
         height: '44%',
         alignItems: 'center',
+        marginBottom: 30,
+        justifyContent: 'space-evenly',
         justifyContent: 'center',
     },
     textInput: {
@@ -73,35 +81,41 @@ const styles = StyleSheet.create({
         opacity: 50,
         padding: 15,
         borderWidth: 1,
-        marginTop: 15,
+        marginTop: '10%',
         marginBottom: 15,
-        borderColor: '#757083'
+        top: 5,
+        borderColor: "#757083"
     },
     chooseBackgroundColor: {
         flex: 1,
         fontSize: 16,
         fontWeight: '300',
         color: '#757083',
+        marginTop: 20,
     },
     colorButtonsBox: {
         flexDirection: 'row',
-        justifyContent: 'space-between',
-        margin: 20
+        width: '80%',
+        justifyContent: "space-between",
+        top: 5,
+        bottom: 5
     },
     colorButton: {
         width: 50,
         height: 50,
         borderRadius: 25,
-        margin: 5
+        margin: 10
     },
     selected: {
         borderColor: 'black',
         borderWidth: 1
     },
     button: {
-        alignItems: 'center',
         backgroundColor: '#757083',
-        padding: 10,
+        padding: 15,
+        margin: 20,
+        alignItems: 'center',
+        width: '88%'
     },
     buttonText: {
         fontSize: 16,
